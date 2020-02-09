61fad2a8004e2692d8a72d9fe86d44c682cfaf08
Facundo Gauna <fgauna12@gmail.com>
2020-02-09 11:46:06 -0500

    First iteration of the plan

---
+++ iac/modules/aks-cluster.tf/main.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+}
+
+terraform {
+  backend "azurerm" {}
+}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = var.node_size
+  }
+
+  service_principal {
+    client_id     = var.service_principal.client_id
+    client_secret = var.service_principal.client_secret
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+
---
+++ iac/modules/aks-cluster.tf/variables.tf
@@ -0,0 +1,32 @@
+variable "cluster_name" {
+
+}
+
+variable "resource_group_name" {
+
+}
+
+variable "environment" {
+
+}
+
+
+variable "node_count" {
+  default = 3
+}
+
+variable "node_size" {
+  default = "Standard_D2_v2"
+}
+
+variable "service_principal" {
+  type = object({
+    client_id     = string
+    client_secret = string
+  })
+  description = "The service principal to use"
+}
+
+
+
+
