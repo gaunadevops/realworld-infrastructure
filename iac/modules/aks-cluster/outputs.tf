4be3d15d03f0e4ae3ccf25825bb3effcfc17073a
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-11 08:58:21 -0500

    Added kubernetes provider

--- iac/modules/aks-cluster/outputs.tf
+++ iac/modules/aks-cluster/outputs.tf
@@ -1,5 +1,25 @@
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
+  value = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+}
+
+output "cluster_ca_certificate" {
+  value = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+}
+
+output "client_key" {
+  value = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+}
+
+output "host" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+}
+
+output "username" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.username
+}
+
+output "password" {
+  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.password
 }
 
 output "kube_config" {
---
+++ iac/modules/k8s-gitops/variables.tf
@@ -0,0 +1,23 @@
+variable "host" {
+
+}
+
+variable "username" {
+
+}
+
+variable "password" {
+
+}
+
+variable "client_certificate" {
+
+}
+
+variable "client_key" {
+
+}
+
+variable "cluster_ca_certificate" {
+    
+}
