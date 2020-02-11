1e96b61206e8701b45d84971bdd3fd35e5da514a
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-11 09:11:51 -0500

    fixing the decode issue again. telling kubernetes provider to not use the config file

--- iac/modules/aks-cluster/outputs.tf
+++ iac/modules/aks-cluster/outputs.tf
@@ -1,13 +1,13 @@
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
+  value = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
 }
 
 output "cluster_ca_certificate" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate
+  value = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
 }
 
 output "client_key" {
-  value = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key
+  value = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
 }
 
 output "host" {
--- iac/modules/k8s-gitops/main.tf
+++ iac/modules/k8s-gitops/main.tf
@@ -1,10 +1,12 @@
 provider "kubernetes" {
-  host                   = "${var.host}"
-  username               = "${var.username}"
-  password               = "${var.password}"
-  client_certificate     = "${base64decode(var.client_certificate)}"
-  client_key             = "${base64decode(var.client_key)}"
-  cluster_ca_certificate = "${base64decode(var.cluster_ca_certificate)}"
+  host                   = var.host
+  username               = var.username
+  password               = var.password
+  client_certificate     = var.client_certificate
+  client_key             = var.client_key
+  cluster_ca_certificate = var.cluster_ca_certificate
+
+  load_config_file = "false"
 }
 
 resource "kubernetes_namespace" "app_namespace" {
