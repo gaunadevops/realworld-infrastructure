6ae358802957cd8d1b9350ddfd3bc3aaccae2d30
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-12 09:47:22 -0500

    added the stable repository for helm 3

--- iac/modules/k8s-gitops/helm.tf
+++ iac/modules/k8s-gitops/helm.tf
@@ -1,4 +1,10 @@
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
 resource "helm_release" "redis" {
   name  = "redis"
   chart = "stable/redis"
+  repository = data.helm_repository.stable.metadata[0].name
 }
