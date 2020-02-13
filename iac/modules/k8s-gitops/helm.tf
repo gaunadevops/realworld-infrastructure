d7149a1e1c57d7f98a136ce0b1d73614ade4074d
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-13 08:35:01 -0500

    disabling installation of crds

--- iac/modules/k8s-gitops/helm.tf
+++ iac/modules/k8s-gitops/helm.tf
@@ -34,8 +34,8 @@ resource "helm_release" "helm_operator" {
   repository = data.helm_repository.fluxcd.metadata.0.name
 }
 
-resource "null_resource" "flux_helm_crd" {
-  provisioner "local-exec" {
-    command = "kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/flux-helm-release-crd.yaml"
-  }
-}
+# resource "null_resource" "flux_helm_crd" {
+#   provisioner "local-exec" {
+#     command = "kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/flux-helm-release-crd.yaml"
+#   }
+# }
