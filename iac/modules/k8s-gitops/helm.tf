9aa845d3c9d382bcfa5c4ff35cefcdc82c3f3843
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-13 08:25:39 -0500

    abandoning the fluxcd module

--- iac/modules/k8s-gitops/helm.tf
+++ iac/modules/k8s-gitops/helm.tf
@@ -1,35 +1,41 @@
-# data "helm_repository" "fluxcd" {
-#     name = "stable"
-#     url  = "https://charts.fluxcd.io" 
-# }
+data "helm_repository" "fluxcd" {
+    name = "fluxcd"
+    url  = "https://charts.fluxcd.io" 
+}
 
-# resource "helm_release" "flux" {
-#   name  = "flux"
-#   namespace = "flux"
-#   chart = "fluxcd/flux"
+resource "helm_release" "flux" {
+  name  = "flux"
+  namespace = "flux"
+  chart = "fluxcd/flux"
 
-#   set {
-#       name = "git.url"
-#       value = "git@github.com:fgauna12"
-#   }
+  set {
+      name = "git.url"
+      value = "git@github.com:fgauna12/flux-get-started"
+  }
 
-#   repository = data.helm_repository.fluxcd.metadata.0.name
-# }
+  repository = data.helm_repository.fluxcd.metadata.0.name
+}
 
-# resource "helm_release" "flux" {
-#   name  = "helm-operator"
-#   namespace = "flux"
-#   chart = "fluxcd/helm-operator"
+resource "helm_release" "flux" {
+  name  = "helm-operator"
+  namespace = "flux"
+  chart = "fluxcd/helm-operator"
 
-#   set {
-#       name = "git.ssh.secretName"
-#       value = "flux-git-deploy"
-#   }
+  set {
+      name = "git.ssh.secretName"
+      value = "flux-git-deploy"
+  }
 
-#   set {
-#       name = "helm.versions"
-#       value = "v3"
-#   }
+  set {
+      name = "helm.versions"
+      value = "v3"
+  }
 
-#   repository = data.helm_repository.fluxcd.metadata.0.name
-# }
+  repository = data.helm_repository.fluxcd.metadata.0.name
+}
+
+resource "null_resource" "flux_helm_crd" {
+  provisioner "local-exec" {
+    command = "kubectl apply -f https://raw.githubusercontent.com/fluxcd/helm-operator/master/deploy/flux-helm-release-crd.yaml"
+  }
+}
