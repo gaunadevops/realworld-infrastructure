83bb52afd04491bff4ba6b652e57d3d0833d23f7
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-16 16:00:52 -0500

    Making things cleaner

--- iac/staging/cluster.tf
+++ iac/staging/cluster.tf
@@ -1,9 +1,9 @@
 locals {
-  environment_name = "staging"
+  environment_name    = "staging"
   resource_group_name = "rg-realworld-${local.environment_name}-001"
-  cluster_name = "azaks-realworld-${local.environment_name}-001"
-  location = "eastus"
-  node_count = 3
+  cluster_name        = "azaks-realworld-${local.environment_name}-001"
+  location            = "eastus"
+  node_count          = 3
 }
 
 provider "azurerm" {
@@ -11,16 +11,16 @@ provider "azurerm" {
 }
 
 module "cluster" {
-    source = "../modules/aks-cluster"
+  source = "../modules/aks-cluster"
 
-    location = local.location
-    node_count = local.node_count
-    environment = local.environment_name
-    resource_group_name = local.resource_group_name
-    aks_service_principal_client_id = var.aks_service_principal_client_id
-    aks_service_principal_client_secret = var.aks_service_principal_client_secret
-    cluster_name = local.cluster_name
-    dns_prefix = local.cluster_name
+  location                            = local.location
+  node_count                          = local.node_count
+  environment                         = local.environment_name
+  resource_group_name                 = local.resource_group_name
+  aks_service_principal_client_id     = var.aks_service_principal_client_id
+  aks_service_principal_client_secret = var.aks_service_principal_client_secret
+  cluster_name                        = local.cluster_name
+  dns_prefix                          = local.cluster_name
 }
 
 provider "kubernetes" {
@@ -29,7 +29,7 @@ provider "kubernetes" {
   password               = module.cluster.password
   client_certificate     = module.cluster.client_certificate
   client_key             = module.cluster.client_key
-  cluster_ca_certificate = module.cluster.cluster_ca_certificate  
+  cluster_ca_certificate = module.cluster.cluster_ca_certificate
 
   load_config_file = false
 }
@@ -50,5 +50,5 @@ provider "helm" {
 }
 
 module "gitops" {
-    source = "../modules/aks-cluster/modules/k8s-gitops"
+  source = "../modules/aks-cluster/modules/k8s-gitops"
 }
