734b1d06681f92b3b050598f297978b72c3d54d6
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-19 13:28:30 -0500

    not using the kube config file and instead will leverage the name of the cluster and the resource group name

--- iac/staging/outputs.tf
+++ iac/staging/outputs.tf
@@ -1,3 +1,7 @@
-output "kube_config" {
-    value = module.cluster.kube_config
+output "resource_group_name" {
+    value = local.resource_group_name
+}
+
+output "cluster_name" {
+    value = local.cluster_name
 }
