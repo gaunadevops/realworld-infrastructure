583c2446107fc65c9c43442906ba27362eba1a20
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-19 16:49:19 -0500

    creating flux namespace through the azure cli

--- iac/staging/main.tf
+++ iac/staging/main.tf
@@ -26,33 +26,3 @@ module "cluster" {
   cluster_name                        = local.cluster_name
   dns_prefix                          = local.cluster_name
 }
-
-provider "kubernetes" {
-  host                   = module.cluster.host
-  username               = module.cluster.username
-  password               = module.cluster.password
-  client_certificate     = module.cluster.client_certificate
-  client_key             = module.cluster.client_key
-  cluster_ca_certificate = module.cluster.cluster_ca_certificate
-
-  load_config_file = false
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = module.cluster.host
-    username               = module.cluster.username
-    password               = module.cluster.password
-    client_certificate     = module.cluster.client_certificate
-    client_key             = module.cluster.client_key
-    cluster_ca_certificate = module.cluster.cluster_ca_certificate
-
-    load_config_file = false
-  }
-
-  debug = true
-}
-
-module "gitops" {
-  source = "../modules/k8s-gitops"
-}
