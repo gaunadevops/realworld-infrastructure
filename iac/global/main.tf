087e7a49bee9b81a0414b2975f677f35d94bb594
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-19 16:28:24 -0500

    fixing my last mistake

--- iac/global/main.tf
+++ iac/global/main.tf
@@ -6,7 +6,7 @@ provider "github" {
 # Add deploy key
 resource "github_repository_deploy_key" "staging_flux_deploy_key" {
   title      = "Real World Staging Flux"
-  repository = "fgauna12/flux-get-started"
+  repository = "flux-get-started"
   key        = var.staging_ssh_public_key
   read_only  = false
 }
