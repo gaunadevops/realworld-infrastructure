59d046e43d74ac1cbb8cc0c4eef000bf20d70a1d
Facundo Gauna <facundo.gauna@newsignature.com>
2020-02-23 21:08:46 -0500

    renaming to try to fool it

--- iac/modules/aks-cluster/main.tf
+++ iac/modules/aks-cluster/main.tf
@@ -25,14 +25,14 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   }
 }
 
-data "azuread_service_principal" "aks_principal" {
+data "azuread_service_principal" "aks_service_principal" {
   application_id = var.aks_service_principal_client_id
 }
 
 resource "azurerm_role_assignment" "acrpull_role" {
   scope                            = var.acr_resource_id
   role_definition_name             = "AcrPull"
-  principal_id                     = data.azuread_service_principal.aks_principal.id
+  principal_id                     = data.azuread_service_principal.aks_service_principal.id
 }
 
 
