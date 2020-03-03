4f6c0aa881ef49d1d23d522ee8765096def29097
Facundo Gauna <facundo.gauna@newsignature.com>
2020-03-03 06:24:52 -0800

    enabling admin accoutn since it's needed by some things

--- iac/global/container-registry/main.tf
+++ iac/global/container-registry/main.tf
@@ -8,5 +8,5 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   sku                      = "Standard"
-  admin_enabled            = false
+  admin_enabled            = true
 }
